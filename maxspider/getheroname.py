8823dc11bc66198993f8be284d46ddb52ee267d7
disoul <starky_ds@sina.cn>
2015-09-22 00:10:10 +0800

    sort hero

--- maxspider/getheroname.py
+++ maxspider/getheroname.py
@@ -8,9 +8,20 @@ def main():
     for index,name in enumerate(names):
         names[index] = name[4:]
 
-    with open('../app/public/js/heronames.json','w') as f:
-        json.dump({'heronames': names}, f)
-        print('OK');
+    for i in xrange(110):
+        change_index = -1
+        while change_index == -1:
+            name = raw_input('Heroname: ')
+            for index,heroname in enumerate(names):
+                if name == heroname:
+                    change_index = index
+                    print 'getHero'
+                    break
+        names[i], names[change_index] = names[change_index], names[i]
+        with open('../app/public/js/heronames.json','w') as f:
+            json.dump({'heronames': names}, f)
+            print('OK');
+
 
 
 if __name__ == '__main__':
