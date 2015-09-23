b6be19bee5d749a204e91861dd000cbe92ce52b5
disoul <starky_ds@sina.cn>
2015-09-23 09:51:51 +0800

    适配

--- app/public/js/SuggestedViewController.js
+++ app/public/js/SuggestedViewController.js
@@ -1,6 +1,6 @@
 app.controller('SuggestedViewController', ['$scope', 
     function($scope){
-        $scope.herolist = [1,2,3,4,5,6,7,8,9,0,1,2,3,4,5].map(function(ele) {
+        $scope.herolist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(function(ele) {
             return {
                 name: '',
                 imgSrc: './public/image/nohero.jpg'
