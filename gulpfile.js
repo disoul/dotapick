e51e0e13cabc3aaeb6ff43d89b0b84a4d954ef81
disoul <starky_ds@sina.cn>
2015-10-14 16:26:08 +0800

    add minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp'),
     compass = require('gulp-compass'),
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    del = require('del'),
 	path = require('path');
 
 gulp.task('compass',function(){
@@ -10,6 +15,28 @@ gulp.task('compass',function(){
 		sass: 'sass',
 		image: 'image'
 	}));
+});
+
+gulp.task('minifycss', function() {
+    return gulp.src('./app/public/css/index.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./app/public/minify'));
+});
+
+gulp.task('minifyjs', function() {
+    return gulp.src('./app/public/js/*.js')
+        .pipe(concat('dotapick.js'))
+        .pipe(gulp.dest('./app/public/minify'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./app/public/minify'));
+});
+
+gulp.task('minify', function(path) {
+    del(['./app/public/minify/**']).then(function(){
+        console.log('clean complete');
+        gulp.start('minifycss', 'minifyjs');
+    })
 });
 
 gulp.task('sass:watch',function(){
